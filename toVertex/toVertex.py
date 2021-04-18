1370de23b55fc9c0f400cda6fd6a8733007fdbf1
Shota Chiyomatsu <63567522+ShotaChiyomatsu@users.noreply.github.com>
2021-04-18 13:28:46 +0900

    Update toVertex.py

--- toVertex/toVertex.py
+++ toVertex/toVertex.py
@@ -5,7 +5,6 @@ from PySide2.QtCore import *
 from PySide2.QtCore import Qt
 from shiboken2 import wrapInstance
 from maya import OpenMayaUI
-import pymel.core.datatypes as dt
 #-------------------------------------------------------
 # Base Window
 #-------------------------------------------------------
@@ -14,120 +13,112 @@ def baseWindow():
     return wrapInstance(long(mainWindow), QWidget)
 #-------------------------------------------------------
 # Main
-#-------------------------------------------------------  
-backColor = "background:qlineargradient(x2:1, y1:0, x2:1, y2:1, stop:0.4#659999 stop:0.8#f4791f)"
-widgetColor = "background:rgba(158, 200, 226, 0.3);color:#f5f5f5;border-color:#f5f5f5;font-weight:bold"
-                  
+#-------------------------------------------------------                   
 class Gui(QDialog):
     
     def __init__(self, parent=baseWindow()):
         super(Gui, self).__init__(parent)
         self.setWindowFlags(Qt.Dialog|Qt.WindowCloseButtonHint)
-        self.setWindowTitle("To Vertex ver2.0")
+        self.setWindowTitle("To Vertex Tool")
         self.design()
         self.alphabetList()
-    #---------------------------------------------------
-    # Design
-    #---------------------------------------------------
+        self.prefCheck()
+
     def design(self):
-        self.setStyleSheet(backColor)
-        self.outputLayout = QGridLayout(self)
+        self.outputLayout = QVBoxLayout(self)
+        self.upperLayout = QHBoxLayout()
+        self.lowerLayout = QHBoxLayout()
         self.widgets = []
-        for i in range(4):
-            # Widget
+        for i in range(5):
+            # ウィジェットの作成
             if i == 0:
-                self.widgets.append(QComboBox())
-                self.widgets[0].addItems(["Locator", "Joint"])
-                self.widgets[0].setFixedSize(80, 25)
+                self.widgets.append(QCheckBox("Chain Mode"))
+                self.widgets[i].setChecked(True)
             elif i == 1:
-                self.widgets.append(QLineEdit())
+                self.widgets.append(QLabel("Type :"))
             elif i == 2:
                 self.widgets.append(QComboBox())
-                self.widgets[2].addItems(["0-9", "A-Z", "a-z"])
-                self.widgets[2].setFixedSize(50, 25)
+                self.widgets[i].addItems(["Joint","Locator"])
+                self.widgets[i].setFixedSize(80, 25)
             elif i == 3:
-                self.widgets.append(QPushButton("Chain"))
-            
-            # Layout
+                self.widgets.append(QLineEdit())
+            elif i == 4:
+                self.widgets.append(QComboBox())
+                self.widgets[i].addItems(["0-9", "A-Z", "a-z"])
+                self.widgets[i].setFixedSize(50, 25)
+            # レイアウト
             if i <= 2:
-                self.outputLayout.addWidget(self.widgets[i], 0, i)
-            elif i == 3:
-                self.outputLayout.addWidget(self.widgets[i], 1, 0, 1, 3)
-   
-            self.widgets[i].setStyleSheet(widgetColor)
-        
-        # Connect
-        self.widgets[1].returnPressed.connect(self.create)
-        self.widgets[3].clicked.connect(self.chain)
-    #---------------------------------------------------
-    # Processing
-    #---------------------------------------------------
+                self.upperLayout.addWidget(self.widgets[i])
+            else:
+                self.lowerLayout.addWidget(self.widgets[i])
+            # カラー
+            self.widgets[i].setStyleSheet("font-weight:bold")
+        self.outputLayout.addLayout(self.upperLayout)
+        self.outputLayout.addLayout(self.lowerLayout)
+        # 接続
+        self.widgets[3].returnPressed.connect(self.create)
+
     def alphabetList(self):
         self.ALPHALIST = [chr(i) for i in range(65,65+26)]
         self.alphalist = [chr(i) for i in range(97,97+26)]  
 
+    def prefCheck(self):
+        # 設定の確認
+        if not cmds.selectPref(q=True, tso=True):
+            cmds.selectPref(tso=True) 
+    
     def create(self):
         cmds.undoInfo(openChunk=True)
-        selection = cmds.ls(sl=True)
-        items = selection[0].split(".", 1)
-        # Get Components
-        component = []
-        try:
-            vtxList = cmds.filterExpand(sm=31)
-            for i in range(len(vtxList)):
-                vtx = cmds.xform(vtxList[i], ws=True, q=True, t=True)
-                component.append(dt.Vector(vtx[0], vtx[1], vtx[2]))
-                 
-        except:
-            cvList = cmds.filterExpand(sm=28)
-            for i in range(len(cvList)):
-                cv = cmds.xform(cvList[i], ws=True, q=True, t=True)
-                component.append(dt.Vector(cv[0], cv[1], cv[2]))
-            
-        # Error
-        if len(component) > 25 and self.widgets[2].currentText() == "A-Z" or \
-            len(component) > 25 and self.widgets[2].currentText() == "a-z":
-            
+        selection = cmds.ls(os=True, fl=True)
+        selection_pos = []
+        create_obj = []
+        # 選択しているコンポーネントが２６個以内か確認
+        if len(selection) > 25 and self.widgets[4].currentText() == "A-Z" or \
+            len(selection) > 25 and self.widgets[4].currentText() == "a-z":
             print("When using Alphabet for suffixes Must not exceed 26 vertices"),
-            
-        # Create
+        
         else:
-            for i in range(len(component)):
-                cmds.select(None)
-                if self.widgets[0].currentText() == "Locator":
-                    if self.widgets[2].currentText() == "0-9":
-                        cmds.spaceLocator(n=self.widgets[1].text() + str(i), a=True)
-                        cmds.move(component[i][0], component[i][1], component[i][2]) 
-                    elif self.widgets[2].currentText() == "A-Z":
-                        cmds.spaceLocator(n=self.widgets[1].text() + self.ALPHALIST[i], a=True)
-                        cmds.move(component[i][0], component[i][1], component[i][2])
-                    elif self.widgets[2].currentText() == "a-z":
-                        cmds.spaceLocator(n=self.widgets[1].text() + self.alphalist[i], a=True)
-                        cmds.move(component[i][0], component[i][1], component[i][2])
-                
-                elif self.widgets[0].currentText() == "Joint":
-                    if self.widgets[2].currentText() == "0-9":
-                        cmds.joint(n=self.widgets[1].text() + str(i), 
-                        p=[component[i][0], component[i][1], component[i][2]])
-                    elif self.widgets[2].currentText() == "A-Z":
-                        cmds.joint(n=self.widgets[1].text() + self.ALPHALIST[i], 
-                        p=[component[i][0], component[i][1], component[i][2]])
-                    elif self.widgets[2].currentText() == "a-z":
-                        cmds.joint(n=self.widgets[1].text() + self.alphalist[i], 
-                        p=[component[i][0], component[i][1], component[i][2]])
-            
-            cmds.select(None)            
-            cmds.undoInfo(closeChunk=True)
+            # 位置の取得
+            for i in range(len(selection)):
+                selection_pos.append(cmds.pointPosition(selection[i]))
 
-    def chain(self):
-        cmds.undoInfo(openChunk=True)
-        selection = cmds.ls(sl=True)
-        try:
-            for i in range(len(selection)-1):
-                cmds.parent(selection[i+1], selection[i])
-        except:
-            cmds.undo()
-            print("There is a node with the same name"), 
+            # オブジェクトを配置
+            for i in range(len(selection)):
+                cmds.select(None)
+                # ジョイントを配置
+                if self.widgets[2].currentText() == "Joint":
+                    if self.widgets[4].currentText() == "0-9":
+                        create_obj.append(cmds.joint(n=self.widgets[3].text() + str(i), 
+                        p=[selection_pos[i][0], selection_pos[i][1], selection_pos[i][2]]))
+                    
+                    elif self.widgets[4].currentText() == "A-Z":
+                        create_obj.append(cmds.joint(n=self.widgets[3].text() + self.ALPHALIST[i], 
+                        p=[selection_pos[i][0], selection_pos[i][1], selection_pos[i][2]]))
+                    
+                    elif self.widgets[4].currentText() == "a-z":
+                        create_obj.append(cmds.joint(n=self.widgets[3].text() + self.alphalist[i], 
+                        p=[selection_pos[i][0], selection_pos[i][1], selection_pos[i][2]]))
+                
+                # ロケーターを配置
+                elif self.widgets[2].currentText() == "Locator":
+                    if self.widgets[4].currentText() == "0-9":
+                        create_obj.append(cmds.spaceLocator(n=self.widgets[3].text() + str(i), a=True))
+                        cmds.move(selection_pos[i][0], selection_pos[i][1], selection_pos[i][2])
+                    
+                    elif self.widgets[4].currentText() == "A-Z":
+                        create_obj.append(cmds.spaceLocator(n=self.widgets[3].text() + self.ALPHALIST[i], a=True))
+                        cmds.move(selection_pos[i][0], selection_pos[i][1], selection_pos[i][2])
+                    
+                    elif self.widgets[4].currentText() == "a-z":
+                        create_obj.append(cmds.spaceLocator(n=self.widgets[3].text() + self.alphalist[i], a=True))
+                        cmds.move(selection_pos[i][0], selection_pos[i][1], selection_pos[i][2])
+
+            # オブジェクトを鎖状に親子付け
+            if self.widgets[0].isChecked():
+                for i in range(len(create_obj)-1):
+                    cmds.parent(create_obj[i+1], create_obj[i])
+
+        cmds.select(None)
         cmds.undoInfo(closeChunk=True)
 #-------------------------------------------------------
 # Show
